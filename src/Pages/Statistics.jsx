141b6847769b7dab18850d74d7fc45ad81965bd5
data-yuyu <121992743+data-yuyu@users.noreply.github.com>
2023-05-26 13:44:11 +0900

    Update Statistics.jsx
    test

--- src/Pages/Statistics.jsx
+++ src/Pages/Statistics.jsx
@@ -4,6 +4,7 @@ export default function () {
   return (
     <>
       <div className="font-bold text-slate-600">統計</div>
+      <div>統計だだ！</div>
       <Link to="/allTrainings">トレーニング</Link>
     </>
   );
