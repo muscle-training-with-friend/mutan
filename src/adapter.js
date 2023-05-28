580ad2f7cda17cdc49f6c1cfb5e276b33ecee614
u120022 <u120022@st.pu-toyama.ac.jp>
2023-05-28 23:56:04 +0900

    :sparkles: feat: adapter for communicating with back-end web api

---
+++ src/adapter.js
@@ -0,0 +1,314 @@
+const BASE_URL = "https://mutan.azurewebsites.net";
+
+/**
+ * ユーザの確認・取得
+ *
+ * @param {{ token: string }} cx
+ *
+ * @returns {{ token: string }}
+ */
+export async function getUser(cx) {
+  const reply = await fetch(BASE_URL + "/get_user", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(cx)
+  });
+  const json = await reply.json();
+
+  console.log(json);
+  return json;
+}
+
+/**
+ * ユーザの生成
+ *
+ * @returns {{ token: string }}
+ */
+export async function createUser() {
+  const reply = await fetch(BASE_URL + "/create_user");
+  const json = await reply.json();
+
+  console.log(json);
+  return json;
+}
+
+/**
+ * ユーザの削除
+ *
+ * @param {{ token: string }} cx
+ */
+export async function deleteUser(cx) {
+  await fetch(BASE_URL + "/delete_user", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(cx)
+  });
+}
+
+/**
+ * すべてのタスクの取得
+ *
+ * @param {{ user_token: string }} cx
+ *
+ * @returns {{ 
+ *   id: number,
+ *   name: string,
+ *   description: string?,
+ *   training_instances: {
+ *     id: number,
+ *     training_id: number,
+ *     name: string,
+ *     description: string?,
+ *     weight_value: f64,
+ *     count_value: i32,
+ *   }[],
+ * }[]}
+ */
+export async function getTasks(cx) {
+  const reply = await fetch(BASE_URL + "/get_tasks", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(cx)
+  });
+  const json = await reply.json();
+
+  console.log(json);
+  return json;
+}
+
+/**
+ * 単体のタスクの取得
+ *
+ * @param {{ user_token: string, id: number }} cx
+ *
+ * @returns {{ 
+ *   id: number,
+ *   name: string,
+ *   description: string?,
+ *   training_instances: {
+ *     id: number,
+ *     training_id: number,
+ *     name: string,
+ *     description: string?,
+ *     weight_value: f64,
+ *     count_value: i32,
+ *   }[],
+ * }}
+ */
+export async function getTask(cx) {
+  const reply = await fetch(BASE_URL + "/get_task", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(cx)
+  });
+  const json = await reply.json();
+
+  console.log(json);
+  return json;
+}
+
+/**
+ * タスクの追加
+ *
+ * @param {{
+ *   user_token: string,
+ *   name: string,
+ *   description: string?,
+ *   training_instances: {
+ *     training_id: number,
+ *     weight_value: number,
+ *     count_value: number,
+ *   }[],
+ * }} cx
+ */
+export async function createTask(cx) {
+  await fetch(BASE_URL + "/create_task", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(cx)
+  });
+}
+
+/**
+ * タスクの削除
+ *
+ * @param {{ user_token: string, id: number }} cx
+ */
+export async function deleteTask(cx) {
+  await fetch(BASE_URL + "/delete_task", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(cx)
+  });
+}
+
+/**
+ * すべての進行中のタスクの取得
+ *
+ * @param {{ user_token: string }} cx
+ *
+ * @returns {{ 
+ *   id: number,
+ *   task_id: number,
+ *   name: string,
+ *   description: string?,
+ *   training_instances: {
+ *     id: number,
+ *     training_id: number,
+ *     name: string,
+ *     description: string?,
+ *     weight_value: f64,
+ *     count_value: i32,
+ *   }[],
+ * }[]}
+ */
+export async function getTaskInstances(cx) {
+  const reply = await fetch(BASE_URL + "/get_task_instances", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(cx)
+  });
+  const json = await reply.json();
+
+  console.log(json);
+  return json;
+}
+
+/**
+ * 単体の進行中のタスクを取得
+ * @param {{ user_token: string, id: number }} cx
+ *
+ * @returns {{ 
+ *   id: number,
+ *   task_id: number,
+ *   name: string,
+ *   description: string?,
+ *   training_instances: {
+ *     id: number,
+ *     training_id: number,
+ *     name: string,
+ *     description: string?,
+ *     weight_value: f64,
+ *     count_value: i32,
+ *   }[],
+ * }}
+ */
+export async function getTaskInstance(cx) {
+  const reply = await fetch(BASE_URL + "/get_task_instance", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(cx)
+  });
+  const json = await reply.json();
+
+  console.log(json);
+  return json;
+}
+
+/**
+ * 進行中のタスクを追加
+ *
+ * @param {{ user_token: string, task_id: number }} cx
+ */
+export async function createTaskInstance(cx) {
+  const reply = await fetch(BASE_URL + "/create_task_instance", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(cx)
+  });
+  const json = await reply.json();
+
+  console.log(json);
+  return json;
+}
+
+/**
+ * 進行中のタスクを進行
+ *
+ * @param {{
+ *   user_token: string,
+ *   task_id: number,
+ *   progress_value: number,
+ * }} cx
+ */
+export async function proceedTaskInstance(cx) {
+  const reply = await fetch(BASE_URL + "/proceed_task_instance", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(cx)
+  });
+  const json = await reply.json();
+
+  console.log(json);
+  return json;
+}
+
+/**
+ * 進行中のタスクを削除
+ *
+ * @param {{
+ *   user_token: string,
+ *   id: number,
+ * }} cx
+ */
+export async function proceedTaskInstance(cx) {
+  const reply = await fetch(BASE_URL + "/delete_task_instance", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(cx)
+  });
+  const json = await reply.json();
+
+  console.log(json);
+  return json;
+}
+
+/**
+ * すべてのトレーニングを取得
+ *
+ * @param {{ offset: number, size: number }} cx
+ *
+ * @returns {{
+ *   id: number,
+ *   name: string,
+ *   description: string?,
+ *   default_weight_value: number,
+ *   default_count_value: number,
+ * }}
+ */
+export async function getTrainings(cx) {
+  const reply = await fetch(BASE_URL + "/get_trainings", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(cx)
+  });
+  const json = await reply.json();
+
+  console.log(json);
+  return json;
+}
+
+/**
+ * 単体のトレーニングを取得
+ *
+ * @param {{ id: number }} cx
+ *
+ * @returns {{
+ *   id: number,
+ *   name: string,
+ *   description: string?,
+ *   default_weight_value: number,
+ *   default_count_value: number,
+ * }}
+ */
+export async function getTrainings(cx) {
+  const reply = await fetch(BASE_URL + "/get_training", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(cx)
+  });
+  const json = await reply.json();
+
+  console.log(json);
+  return json;
+}
