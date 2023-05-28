e42ee22dca8c0b831c6f524599b2e447e5e42eeb
nayuchax <Ayumuwwwwwwwwww@gmail.com>
2023-05-28 17:08:38 +0900

    Update Navbar.jsx
    左右に変な余白ある
    画面下部に固定している

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function () {
   return (
-    <div className="bg-slate-100">
-      <Link to="/home">ホーム</Link>
-      <Link to="/statistics">統計</Link>
-      <Link to="/tasks">タスク</Link>
-      <Link to="/appearance">着せ替え</Link>
-      <Link to="/friends">友達</Link>
-    </div>
+      <div className="absolute grid grid-cols-5 gap-3 py-6 px-6 bottom-0 bg-gradient-to-r from-theme_color1 to-theme_color4 rounded-t-2xl">
+        <Link to="/home"><div className="flex justify-center mx-1 text-sm">ホーム</div></Link>
+        <Link to="/statistics"><div className="flex justify-center mx-1 text-sm">統計</div></Link>
+        <Link to="/tasks"><div className="flex justify-center mx-1 text-sm">タスク</div></Link>
+        <Link to="/appearance"><div className="flex justify-center mx-1 text-sm">着せ替え</div></Link>
+        <Link to="/friends"><div className="flex justify-center mx-1 text-sm">友達</div></Link>
+      </div>
   );
 }
