967e8b2e6e51c388b7773e27061e5d086b0055d4
nayuchax <Ayumuwwwwwwwwww@gmail.com>
2023-05-28 14:20:19 +0900

    Update Tasks.jsx
    color extend追加
    https://colorhunt.co/palette/c4dfdfd2e9e9e3f4f4f8f6f4

--- src/Pages/Tasks.jsx
+++ src/Pages/Tasks.jsx
@@ -3,44 +3,51 @@ import { Link } from "react-router-dom";
 export default function () {
   return (
     <>
-      <div className="font-bold text-slate-900 text-2xl">最近のタスク</div>
-      <div className="rounded-lg bg-rose-700 my-5 p-6 bg-gradient-to-r from-rose-200 to-rose-600">
-        <Link to="/doneTask">
-          <div className="font-bold text-lg flex justify-center">おすすめのメニュー(1日)</div>
-          <div className="py-3">
-            <div className="text-sm">・ベンチ80kg10回</div>
-            <div className="text-sm">・スクワット30回</div>
-          </div>
-        </Link>
-      </div>
+      <div>
+        <div className="font-bold text-slate-900 text-2xl">最近のタスク</div>
+        <div className="rounded-3xl my-5 p-6 bg-gradient-to-r from-theme_color1 to-theme_color4">
+          <Link to="/doneTask">
+            <div className="font-bold text-lg flex justify-center">おすすめのメニュー(1日)</div>
+            <div className="py-3">
+              <div className="text-sm">・ベンチ80kg10回</div>
+              <div className="text-sm">・スクワット30回</div>
+            </div>
+          </Link>
+        </div>
 
-      <div className="rounded-lg bg-rose-500  my-5 p-6 bg-gradient-to-r from-rose-200 to-rose-600">
-        <Link to="/doneTask">
-          <div className="font-bold text-lg flex justify-center">おすすめのメニュー(1日)</div>
-          <div className="py-3">
-            <div className="text-sm">・ベンチ60kg10回</div>
-            <div className="text-sm">・スクワット20回</div>
-          </div>
-        </Link>
-      </div>
+        <div className="rounded-3xl my-5 p-6 bg-gradient-to-r from-theme_color1 to-theme_color4">
+          <Link to="/doneTask">
+            <div className="font-bold text-lg flex justify-center">おすすめのメニュー(1日)</div>
+            <div className="py-3">
+              <div className="text-sm">・ベンチ60kg10回</div>
+              <div className="text-sm">・スクワット20回</div>
+            </div>
+          </Link>
+        </div>
 
-      <div className="font-bold text-slate-900 text-2xl">すべてのタスク</div>
-      <div className="rounded-lg bg-rose-300 my-5 p-6 bg-gradient-to-r from-rose-200 to-rose-600">
-        <Link to="/doneTask">
-          <div className="font-bold text-lg flex justify-center">おすすめのメニュー(1日)</div>
-          <div className="py-3">
-            <div className="text-sm">・ベンチ40kg10回</div>
-            <div className="text-sm">・スクワット10回</div>
-          </div>
-        </Link>
-      </div>
+        <div className="font-bold text-slate-900 text-2xl">すべてのタスク</div>
+        <div className="rounded-3xl my-5 p-6 bg-gradient-to-r from-theme_color1 to-theme_color4">
+          <Link to="/doneTask">
+            <div className="font-bold text-lg flex justify-center">おすすめのメニュー(1日)</div>
+            <div className="py-3">
+              <div className="text-sm">・ベンチ40kg10回</div>
+              <div className="text-sm">・スクワット10回</div>
+            </div>
+          </Link>
+        </div>
 
-      <div className="rounded-lg bg-rose-100  my-5 p-6">
-        <Link to="/createTask">
-          <div className="text-lg flex justify-center">タスクを追加</div>
-        </Link>
-      </div>
+        <div className="rounded-3xl bg-theme_color2  my-5 p-6">
+          <Link to="/createTask">
+            <div className="flex justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor" className="w-7 h-7">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+              <div className="text-lg ml-1">タスクを追加する</div>
+            </div>
+          </Link>
+        </div>
 
+      </div>
     </>
   );
 }
