7960e4dc419c29c559310f3d1bd9b1aa6b8b3b74
nayuchax <Ayumuwwwwwwwwww@gmail.com>
2023-05-29 17:48:47 +0900

    Update CreateTask.jsx
    bg-color change
    font color change

--- src/Pages/CreateTask.jsx
+++ src/Pages/CreateTask.jsx
@@ -3,32 +3,34 @@ import { Link } from "react-router-dom";
 export default function () {
   return (
     <>
-      <div className="rounded-lg bg-gradient-to-r from-theme_color1 to-theme_color4 mb-6 p-6">
-        <div className="rounded-lg text-lg font-bold bg-white/30 mr-60 px-4 py-2">新規タスク</div>
-        <div className="rounded-lg text-sm bg-white/30 px-4 py-10 mt-6">
-          <div>
-            新しいタスクです。
+      <div className="text-text">
+        <div className="rounded-lg bg-gradient-to-br from-bright_accent to-accent mb-6 p-6">
+          <div className="rounded-lg text-lg font-bold bg-bright_accent mr-60 px-4 py-2">新規タスク</div>
+          <div className="rounded-lg text-sm bg-bright_accent px-4 py-10 mt-6">
+            <div>
+              新しいタスクです。
+            </div>
           </div>
         </div>
+
+        <div className="rounded-lg bg-gradient-to-br from-bright_accent to-accent my-6 p-6">
+          <div className="text-lg font-bold flex justify-center">ベンチ</div>
+          <div className="text-sm">・ベンチ60kg10回</div>
+        </div>
+
+        <div className="rounded-lg bg-gradient-to-br from-bright_accent to-accent my-6 p-6">
+          <div className="text-lg font-bold flex justify-center">ベンチ</div>
+          <div className="text-sm">・ベンチ60kg10回</div>
+        </div>
+
+
+        <Link to="/addTraining">
+          <div className="rounded-lg bg-bright_accent flex justify-center my-6 p-6">
+            <div className="text-lg ">トレーニングを追加</div>
+          </div >
+
+        </Link>
       </div>
-
-      <div className="rounded-lg bg-gradient-to-r from-theme_color1 to-theme_color4 my-6 p-6">
-        <div className="text-lg font-bold flex justify-center">ベンチ</div>
-        <div className="text-sm">・ベンチ60kg10回</div>
-      </div>
-
-      <div className="rounded-lg bg-gradient-to-r from-theme_color1 to-theme_color4 my-6 p-6">
-        <div className="text-lg font-bold flex justify-center">ベンチ</div>
-        <div className="text-sm">・ベンチ60kg10回</div>
-      </div>
-
-
-      <Link to="/addTraining">
-        <div className="rounded-lg bg-theme_color2 flex justify-center my-6 p-6">
-          <div className="text-lg ">トレーニングを追加</div>
-        </div >
-
-      </Link>
 
     </>
   );
