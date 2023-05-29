734677777978b8aa266a3e6cb819a9923d3811b5
nayuchax <Ayumuwwwwwwwwww@gmail.com>
2023-05-29 14:46:02 +0900

    Update tailwind.config.js
    色追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,15 @@ module.exports = {
         theme_color1:'#F8F6F4',
         theme_color2:'#E3F4F4',
         theme_color3:'#D2E9E9',
-        theme_color4:'#C4DFDF'
+        theme_color4:'#C4DFDF',
+        accent:"#FF7152",
+        bright_accent:"#FFC89F",
+        mid_accent:"#FF9D7A",
+        text:"#222",
+        inverted_text:"#FFF",
+        muted_text:"#555",
+        muted_bg:"#EEE",
+        bg:"FFF"
       }
     },
   },
