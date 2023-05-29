3802b0814ec81aad4df8e2ea604e89982b76f48a
takuemon77 <119913180+takuemon77@users.noreply.github.com>
2023-05-29 12:47:47 +0900

    ver.1

--- src/Pages/AllTrainings.jsx
+++ src/Pages/AllTrainings.jsx
@@ -1,9 +1,34 @@
 export default function () {
   return (
     <>
-      <div className="font-bold text-slate-600">トレーニングの一覧</div>
-      <div>aaaa</div>
       
+      <div className="font-bold text-slate-600">トレーニングの一覧</div>
+      
+      
+      <p className="text-xl">胸のトレーニング</p>
+      <div className="rounded-sm bg-red-100 my-5 p-6 ">
+      <div className="flex justify-center"> 
+      <p className="text-2xl bg-red-100 inline-flex rounded ">ベンチプレス</p>
+      </div></div>
+      <div className="text-xl">脚のトレーニング</div>
+      <div className="flex justify-center"> 
+      <p className="text-2xl bg-red-100 inline-flex rounded ">スクワット</p>
+      </div>
+
+      <div className="text-xl">背中のトレーニング</div>
+      <div className="flex justify-center"> 
+      <p className="text-2xl bg-red-100 inline-flex rounded ">チンニング</p>
+      </div>
+      <div className="text-xl">腕のトレーニング</div>
+      <div className="flex justify-center"> 
+      <p className="text-2xl bg-red-100 inline-flex rounded ">アームカール</p>
+      </div>
+      <div className="text-xl">腹筋のトレーニング</div>
+      <div className="flex justify-center"> 
+      <p className="text-2xl bg-red-100 inline-flex rounded ">アクトレの腹筋プログラム</p>
+      </div>
+      
+      a
     </>
   );
 }
