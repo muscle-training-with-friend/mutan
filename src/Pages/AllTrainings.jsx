e7afd4e708748d44897aafb61a968989b132c9f0
takuemon77 <119913180+takuemon77@users.noreply.github.com>
2023-05-30 17:23:47 +0900

    fetchの追加
    とりあえずコミット

--- src/Pages/AllTrainings.jsx
+++ src/Pages/AllTrainings.jsx
@@ -1,34 +1,48 @@
+import React,{useState,useEffect} from "react";
+import { getTrainings } from "../adapter";
+
 export default function () {
+  const [trainings, setTrainings] = useState([]);
+
+  const fn = async () => {
+      const res = await getTrainings({ offset: 0, size: 20 });
+      setTrainings(res);
+  };
+
+  useEffect(() => { fn(); }, []);
+
   return (
     <>
+     
       
       <div className="font-bold text-slate-600">トレーニングの一覧</div>
       
-      
-      <p className="text-xl">胸のトレーニング</p>
-      <div className="rounded-sm bg-red-100 my-5 p-6 ">
-      <div className="flex justify-center"> 
-      <p className="text-2xl bg-red-100 inline-flex rounded ">ベンチプレス</p>
-      </div></div>
-      <div className="text-xl">脚のトレーニング</div>
-      <div className="flex justify-center"> 
-      <p className="text-2xl bg-red-100 inline-flex rounded ">スクワット</p>
+      {trainings.map(training => <div>{training.name}</div>)}
+<div className="text-xl">胸のトレーニング</div>
+<div className="rounded-sm bg-red-100 my-5 p-6 ">
+<div className="flex justify-center"> 
+<div className="text-2xl bg-red-100 inline-flex rounded-md ">ベンチプレス</div>
+</div>
+</div>
+<div className="text-xl">脚のトレーニング</div>
+<div className="flex justify-center"> 
+<div className="text-2xl bg-9 inline-flex rounded ">スクワット</div>
       </div>
 
       <div className="text-xl">背中のトレーニング</div>
       <div className="flex justify-center"> 
-      <p className="text-2xl bg-red-100 inline-flex rounded ">チンニング</p>
+      <div className="text-2xl bg-red-100 inline-flex rounded ">チンニング</div>
       </div>
       <div className="text-xl">腕のトレーニング</div>
       <div className="flex justify-center"> 
-      <p className="text-2xl bg-red-100 inline-flex rounded ">アームカール</p>
+      <div className="text-2xl bg-red-100 inline-flex rounded ">アームカール</div>
       </div>
       <div className="text-xl">腹筋のトレーニング</div>
       <div className="flex justify-center"> 
-      <p className="text-2xl bg-red-100 inline-flex rounded ">アクトレの腹筋プログラム</p>
+      <div className="text-2xl bg-red-100 inline-flex rounded ">アクトレの腹筋プログラム</div>
       </div>
       
-      a
+  
     </>
   );
 }
