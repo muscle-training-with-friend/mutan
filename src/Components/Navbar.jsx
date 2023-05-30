8ced7f22c8db02d05256d7c01d4f276b38b68c8a
nayuchax <Ayumuwwwwwwwwww@gmail.com>
2023-05-30 14:58:25 +0900

    Update Navbar.jsx
    完成！！なのだ！！

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 
 export default function () {
   return (
-    <div className="absolute inset-x-0 bottom-0 grid grid-cols-5 gap-3 mb-8 pt-6 pb-4 mx-4 bg-gradient-to-br from-bright_accent to-accent rounded-2xl">
+    <div className="absolute inset-x-0 bottom-0 grid grid-cols-5 gap-5 mb-8 p-4 mx-4 bg-gradient-to-br from-bright_accent to-accent rounded-2xl">
       <Link to="/home">
         <div className="flex justify-center text-white">
           <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
             rel="stylesheet" />
-          <span class="material-icons">
+          <span className="material-icons" style={{ fontSize: "36px" }}>
             home
           </span>
         </div>
-        <div className="flex justify-center text-sm text-muted_text">
+        <div className="flex justify-center text-sm text-white">
           ホーム
         </div>
       </Link>
@@ -19,11 +19,11 @@ export default function () {
       <Link to="/statistics">
         <div className="flex justify-center text-white">
           <link href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-          <span class="material-icons">
+          <span className="material-icons" style={{ fontSize: "36px" }}>
             leaderboard
           </span>
         </div>
-        <div className="flex justify-center text-sm text-muted_text">
+        <div className="flex justify-center text-sm text-white">
           統計
         </div>
       </Link>
@@ -31,11 +31,11 @@ export default function () {
       <Link to="/tasks">
         <div className="flex justify-center text-white">
           <link href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-          <span class="material-icons">
+          <span className="material-icons" style={{ fontSize: "36px" }}>
             add_task
           </span>
         </div>
-        <div className="flex justify-center text-sm text-muted_text">
+        <div className="flex justify-center text-sm text-white">
           タスク
         </div>
       </Link>
@@ -43,11 +43,11 @@ export default function () {
       <Link to="/appearance">
         <div className="flex justify-center text-white">
           <link href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-          <span class="material-icons">
+          <span className="material-icons" style={{ fontSize: "36px" }}>
             palette
           </span>
         </div>
-        <div className="flex justify-center text-sm text-muted_text">
+        <div className="flex justify-center text-sm text-white">
           着せ替え
         </div>
       </Link>
@@ -55,11 +55,11 @@ export default function () {
       <Link to="/friends">
         <div className="flex justify-center text-white">
           <link href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-          <span class="material-icons">
+          <span className="material-icons" style={{ fontSize: "36px" }}>
             people
           </span>
         </div>
-        <div className="flex justify-center text-sm text-muted_text">
+        <div className="flex justify-center text-sm text-white">
           友達
         </div>
       </Link>
