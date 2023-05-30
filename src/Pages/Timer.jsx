9ec3ed414da24cca33253aeaea54eddd51f324f3
nayuchax <Ayumuwwwwwwwwww@gmail.com>
2023-05-30 13:52:57 +0900

    Update Timer.jsx
    改良版

--- src/Pages/Timer.jsx
+++ src/Pages/Timer.jsx
@@ -13,12 +13,13 @@ export default function () {
         <div className="text-sm my-2">・ベンチ30kg10回</div>
       </div>
 
+      <div className="flex justify-center font-sm pt-16 text-muted_text">timer is...
+        <div className="flex justify-center font-sm">{isRunning ? "running" : "not running"}</div>
+      </div>
       <div className="flex justify-center">
         <div className="rounded-full h-80 w-80 bg-gradient-to-br from-bright_accent to-accent my-6 p-3">
           <div className="rounded-full h-full w-full bg-white">
-            <div className="flex justify-center font-sm pt-16 text-muted_text">timer is...</div>
-            <div className="flex justify-center font-sm">{isRunning ? "running" : "not running"}</div>
-            <div className="flex justify-center text-slate-900 text-5xl font-bold pt-10">
+            <div className="flex justify-center text-slate-900 text-7xl font-bold pt-24">
               {minutes}:{seconds}
             </div>
           </div>
@@ -33,7 +34,7 @@ export default function () {
           <div className="flex justify-center rounded-2xl bg-gradient-to-br from-bright_accent to-accent py-3 z-10 mx-2 shadow-[0_0_64px_0_rgba(0.8,0.9,0.9,0.5)] shadow-[#FF7152]" onClick={reset}>finish</div>
         </div>
       </div>
-      
+
     </>
   );
 }
