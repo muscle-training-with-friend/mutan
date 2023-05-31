6e81c81fdd257a7a4ce2de1313e3642e4b82c2dd
nayuchax <Ayumuwwwwwwwwww@gmail.com>
2023-05-31 21:48:28 +0900

    悩んでる
    とりあえず、トレーニングは出力した。
    ボタンを押した種目をcreatetaskで出力したい。onClickでのイベントハンドラのため、一件ごと値を渡す。
    createtaskですべてのパラメータが必要なためデータをそのまま渡す
    いい方法があればよろしくお願いします

--- src/Pages/AddTraining.jsx
+++ src/Pages/AddTraining.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+import { getTrainings } from "../adapter"
+
 export default function () {
+  const [trainings, setTrainings] = useState([]);
+  const [selects, setSelects] = useState([])
+
+  const fn = async () => {
+    const res = await getTrainings({ offset: 0, size: 20 });
+    setTrainings(res);
+  };
+
+  useEffect(() => { fn(); }, []);
+
+  const onClick = () => {
+    
+  }
+
   return (
     <>
-      <div className="font-bold text-slate-600">トレーニングの追加</div>
+
+      <div className="font-bold text-slate-600">トレーニングの一覧</div>
+
+      {trainings.map(training =>
+        <button onClick={onClick} className="w-full h-auto my-6">
+          <div className="rounded-2xl bg-gradient-to-br from-bright_accent to-accent">
+            <div>{training.name}</div>
+            <div>重量:{training.default_weight_value}</div>
+            <div>回数:{training.default_count_value}</div>
+          </div>
+        </button >
+      )}
     </>
   );
+}
+
+export function SelectTraining(){
+  res = selects;
+  return(res);
 }
