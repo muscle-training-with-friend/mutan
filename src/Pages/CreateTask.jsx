d1a39fda1b597ddbf08e9f96d64fa9995099fe80
nayuchax <Ayumuwwwwwwwwww@gmail.com>
2023-05-31 16:13:45 +0900

    Update CreateTask.jsx
    入力フォームの実装

--- src/Pages/CreateTask.jsx
+++ src/Pages/CreateTask.jsx
@@ -1,15 +1,28 @@
 import { Link } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import { TokenContext } from "../Components/TokenContext";
+import { createTask } from "../adapter";
 
 export default function () {
+
+  const createNewTask = () => {
+    let title = document.getElementById('title');
+    let detail = document.getElementById('detail');
+    console.log("clicked!! title="+title.value+"detail="+detail.value)
+  }
+
   return (
     <>
       <div className="text-text">
         <div className="rounded-lg bg-gradient-to-br from-bright_accent to-accent mb-6 p-6">
-          <div className="rounded-lg text-lg font-bold bg-bright_accent mr-60 px-4 py-2">新規タスク</div>
-          <div className="rounded-lg text-sm bg-bright_accent px-4 py-10 mt-6">
-            <div>
-              新しいタスクです。
-            </div>
+          <input type="text" id="title" placeholder="新規タスク" required className="rounded-lg text-text text-lg font-bold bg-bright_accent mr-60 px-4 py-2"></input>
+          <div className="flex justify-center">
+          <input type="text" id="detail" placeholder="新しいタスクです" required className="rounded-lg text-sm bg-bright_accent px-24 py-10 mt-6"></input>
+          </div>
+          <div className="flex justify-center mt-4">
+            <button onClick={createNewTask} className="bg-bright_accent hover:bg-accent text-text font-bold hover:text-inverted_text py-2 px-4 border border-bright_accent hover:border-transparent rounded">
+              タスクを作成
+            </button>
           </div>
         </div>
 
@@ -22,6 +35,7 @@ export default function () {
           <div className="text-lg font-bold flex justify-center">ベンチ</div>
           <div className="text-sm">・ベンチ60kg10回</div>
         </div>
+
 
 
         <Link to="/addTraining">
