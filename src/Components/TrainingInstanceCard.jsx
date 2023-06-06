ddcb79fb2442c688dbfcd7d38d3bf34f0f96e834
u120022 <u120022@st.pu-toyama.ac.jp>
2023-06-06 17:46:06 +0900

    :art: format: clean!

--- src/Components/TrainingInstanceCard.jsx
+++ src/Components/TrainingInstanceCard.jsx
@@ -1,9 +1,9 @@
-export default function({ training_instance }) {
-	return (
-		<div className="rounded-2xl bg-gradient-to-br from-bright_accent to-accent my-6 p-6">
-			<div className="text-lg font-bold">{training_instance.name}</div>
-			<div className="text-sm">{training_instance.weight} kg</div>
-			<div className="text-sm">{training_instance.times} 回</div>
-		</div>
-	);
+export default function ({ training_instance }) {
+  return (
+    <div className="my-6 rounded-2xl bg-gradient-to-br from-bright_accent to-accent p-6">
+      <div className="text-lg font-bold">{training_instance.name}</div>
+      <div className="text-sm">{training_instance.weight} kg</div>
+      <div className="text-sm">{training_instance.times} 回</div>
+    </div>
+  );
 }
--- src/Pages/CreateTask.jsx
+++ src/Pages/CreateTask.jsx
@@ -6,83 +6,83 @@ import { TokenContext } from "../Components/TokenContext";
 import TrainingInstanceBuilder from "../Components/TrainingInstanceBuilder";
 import { useNavigate } from "react-router-dom";
 
-export default function() {
-	const navigate = useNavigate();
-	const token = useContext(TokenContext);
-	const [form, setForm] = useState({
-		name: "",
-		description: "",
-		training_instances: [],
-	});
-	const [modalVisible, setModalVisible] = useState(false);
+export default function () {
+  const navigate = useNavigate();
+  const token = useContext(TokenContext);
+  const [form, setForm] = useState({
+    name: "",
+    description: "",
+    training_instances: [],
+  });
+  const [modalVisible, setModalVisible] = useState(false);
 
-	const createTaskInstanceFn = (training_instance) => {
-		setForm((prev) => ({
-			...prev,
-			training_instances: [...prev.training_instances, training_instance],
-		}));
-		setModalVisible(false);
-	};
+  const createTaskInstanceFn = (training_instance) => {
+    setForm((prev) => ({
+      ...prev,
+      training_instances: [...prev.training_instances, training_instance],
+    }));
+    setModalVisible(false);
+  };
 
-	const createTaskFn = async () => {
-		const striped = {
-			...form,
-			training_instances: form.training_instances.map((training_instance) => ({
-				training_id: training_instance.id,
-				weight: training_instance.weight,
-				times: training_instance.times,
-			})),
-		};
-		await createTask({ ...striped, token });
+  const createTaskFn = async () => {
+    const striped = {
+      ...form,
+      training_instances: form.training_instances.map((training_instance) => ({
+        training_id: training_instance.id,
+        weight: training_instance.weight,
+        times: training_instance.times,
+      })),
+    };
+    await createTask({ ...striped, token });
 
-		navigate("/tasks");
-	};
+    navigate("/tasks");
+  };
 
-	return (
-		<>
-			<Modal visible={modalVisible} closeFn={(_) => setModalVisible(false)}>
-				<TrainingInstanceBuilder build={createTaskInstanceFn} />
-			</Modal>
+  return (
+    <>
+      <Modal visible={modalVisible} closeFn={(_) => setModalVisible(false)}>
+        <TrainingInstanceBuilder build={createTaskInstanceFn} />
+      </Modal>
 
-			<div className="text-text">
-				<div className="mb-6 rounded-2xl bg-gradient-to-br from-bright_accent to-accent p-6">
-					<input
-						type="text"
-						value={form.name}
-						onChange={(e) =>
-							setForm((prev) => ({ ...prev, name: e.target.value }))
-						}
-						className="w-full rounded-2xl bg-bright_accent p-2"
-						placeholder="タスク名"
-					/>
+      <div className="text-text">
+        <div className="mb-6 rounded-2xl bg-gradient-to-br from-bright_accent to-accent p-6">
+          <input
+            type="text"
+            value={form.name}
+            onChange={(e) =>
+              setForm((prev) => ({ ...prev, name: e.target.value }))
+            }
+            className="w-full rounded-2xl bg-bright_accent p-2"
+            placeholder="タスク名"
+          />
 
-					<textarea
-						rows="3"
-						value={form.description}
-						onChange={(e) =>
-							setForm((prev) => ({ ...prev, description: e.target.value }))
-						}
-						className="mt-6 w-full rounded-2xl bg-bright_accent p-2"
-						placeholder="説明"
-					/>
-				</div>
+          <textarea
+            rows="3"
+            value={form.description}
+            onChange={(e) =>
+              setForm((prev) => ({ ...prev, description: e.target.value }))
+            }
+            className="mt-6 w-full rounded-2xl bg-bright_accent p-2"
+            placeholder="説明"
+          />
+        </div>
 
-				{form.training_instances.map((training_instance) => (
-					<TrainingInstanceCard training_instance={training_instance} />
-				))}
+        {form.training_instances.map((training_instance) => (
+          <TrainingInstanceCard training_instance={training_instance} />
+        ))}
 
-				<div onClick={(_) => setModalVisible(true)}>
-					<div className="my-6 flex justify-center rounded-2xl bg-bright_accent p-6">
-						<div className="text-lg ">トレーニングを追加</div>
-					</div>
-				</div>
+        <div onClick={(_) => setModalVisible(true)}>
+          <div className="my-6 flex justify-center rounded-2xl bg-bright_accent p-6">
+            <div className="text-lg ">トレーニングを追加</div>
+          </div>
+        </div>
 
-				<div onClick={createTaskFn}>
-					<div className="my-6 flex justify-center rounded-2xl bg-bright_accent p-6 bg-gradient-to-br from-bright_accent to-accent hover:bg-accent text-text hover:text-inverted_text">
-						<div className="text-lg font-bold ">完了</div>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+        <div onClick={createTaskFn}>
+          <div className="my-6 flex justify-center rounded-2xl bg-bright_accent bg-gradient-to-br from-bright_accent to-accent p-6 text-text hover:bg-accent hover:text-inverted_text">
+            <div className="text-lg font-bold ">完了</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
