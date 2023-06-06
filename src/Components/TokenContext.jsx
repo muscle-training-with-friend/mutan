492f21235f61b810c6027d694b09868789263ef2
u120022 <u120022@st.pu-toyama.ac.jp>
2023-06-06 17:56:02 +0900

    :construction: recover: no related changes

--- src/Components/TokenContext.jsx
+++ src/Components/TokenContext.jsx
@@ -1,8 +1,8 @@
 import Cookies from "js-cookie";
 import { createContext, useEffect, useState } from "react";
-import { createUser, getUser } from "../adapter.js";
+import {createUser, getUser} from "../adapter.js"
 
-const KEY = "USER_TOKEN";
+const KEY = "USER_TOKEN"
 
 /**
  * @type {React.Context<string | undefined>}
@@ -10,30 +10,28 @@ const KEY = "USER_TOKEN";
 export const TokenContext = createContext(undefined);
 
 export function TokenContextProvider(props) {
-  const [token, setToken] = useState(undefined);
+    const [token, setToken] = useState(undefined);
 
-  const fn = async () => {
-    let token = Cookies.get(KEY);
-    if (token) {
-      try {
-        await getUser({ token });
-      } catch {
-        token = undefined;
-      }
-    } else {
-      const res = await createUser();
-      token = res.token;
-      Cookies.set(KEY, token);
+    const fn = async () => {
+        let token = Cookies.get(KEY);
+        if (token) {
+            try {
+                await getUser({ token });
+            } catch {
+                token = undefined;
+            }
+        } else {
+            const res = await createUser();
+            token = res.token;
+            Cookies.set(KEY, token);
+        }
+        setToken(token);
     }
-    setToken(token);
-  };
-  useEffect(() => {
-    fn();
-  }, []);
-
-  return (
-    <TokenContext.Provider value={token}>
-      {props.children}
-    </TokenContext.Provider>
-  );
+    useEffect(() => { fn(); }, []);
+    
+    return(
+        <TokenContext.Provider value={token}>
+            {props.children}
+        </TokenContext.Provider>
+    );
 }
