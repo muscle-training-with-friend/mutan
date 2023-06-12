09a794383176bc16509b9250a84122de3b785361
nayuchax <Ayumuwwwwwwwwww@gmail.com>
2023-06-12 09:45:37 +0900

    トレーニング出力　追加

--- src/Pages/AllTrainings.jsx
+++ src/Pages/AllTrainings.jsx
@@ -41,18 +41,26 @@ export default function () {
       <button onclick={times_line} className="px-2 py-1 bg-blue-400 text-white font-semibold rounded hover:bg-blue-500">回数順</button>
       <button onclick={latest_line} className="px-2 py-1 bg-blue-400 text-white font-semibold rounded hover:bg-blue-500">最近</button>
 
-      <div>
-        {trainings.length != 0 ? (
-          <>
-            {trainings.map((training) => (
+      <div className="font-bold text-slate-600">・胸のトレーニング</div>
+      <div className="font-bold text-slate-600">・肩のトレーニング</div>
+      <div className="font-bold text-slate-600">・腕のトレーニング</div>
+      <div className="font-bold text-slate-600">・背中のトレーニング</div>
+      <div className="font-bold text-slate-600">・足のトレーニング</div>
+
+      {trainings.length != 0 ? (
+        <>
+          {trainings.map((training) => (
+            <>
               <div className="rounded-2xl bg-gradient-to-br from-bright_accent to-accent my-6 p-6">
                 <div className="font-bold text-xl flex justify-center">{training.name}</div>
-                <div  className="text-sm my-2">{training.weight}kg:{training.times}回</div>
+                <div className="text-sm my-2"> {training.weight}kg </div>
+                <div className="text-sm my-2"> {training.times}回 </div>
               </div>
-            ))}
-          </>
-        ) : undefined}
-      </div>
+            </>
+          ))}
+
+        </>
+      ) : undefined}
     </>
   );
 }
