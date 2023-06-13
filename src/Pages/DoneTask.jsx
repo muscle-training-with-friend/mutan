42aa9489e2ded5e427438efc973f3f9720c2db59
nayuchax <Ayumuwwwwwwwwww@gmail.com>
2023-06-14 00:14:30 +0900

    テストデータ。ガキガキ男にはこれを参照しながらやってもらいたいと思う

--- src/Pages/DoneTask.jsx
+++ src/Pages/DoneTask.jsx
@@ -1,9 +1,32 @@
 import { Link } from "react-router-dom";
+import { getTaskInstance } from "../adapter";
+import { TokenContext } from "../Components/TokenContext";
+import { React, useState, useEffect, useContext } from "react";
 
 export default function () {
+  const token = useContext(TokenContext);
+  const [train, setTrain] = useState([]);
+
+  const fn = async () => {
+    const training = await getTaskInstance({ token });
+    setTrain(training);
+  };
+  useEffect(() => {
+    fn();
+  }, []);
+
+
   return (
     <>
-      <div className="font-bold text-slate-600">タスクの実行</div>
+      {train.length != 0 ? (
+        <>
+          <div className="font-bold text-slate-600">タスクの実行</div>
+          {train.training_instances.map((task) => (
+            <div>{task.name}</div>
+          ))}
+        </>
+      ) : undefined}
+
       <Link to="/timer">トレーニングを開始</Link>
     </>
   );
