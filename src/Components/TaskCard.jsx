be19f2b60da48ac280ea88d3bb1d3619a87e3063
nayuchax <Ayumuwwwwwwwwww@gmail.com>
2023-06-14 00:13:46 +0900

    donetasksにデータを渡すため、進行中のタスクを入力

--- src/Components/TaskCard.jsx
+++ src/Components/TaskCard.jsx
@@ -1,8 +1,20 @@
 import { Link } from "react-router-dom";
+import { React, useState, useEffect, useContext } from "react";
+import { TokenContext } from "../Components/TokenContext";
+import { createTaskInstance } from "../adapter";
+
 
 export default function ({ task }) {
+  const token = useContext(TokenContext);
+
+  const addTraining = () => {
+    createTaskInstance({
+      token,
+      task_id : task.id})
+  }
+
   return (
-    <Link to="/doneTask">
+    <Link onClick={() => addTraining} to="/doneTask">
       <div className="my-5 rounded-3xl bg-gradient-to-br from-bright_accent to-accent p-6">
         <div className="flex justify-center text-lg font-bold">{task.name}</div>
 
